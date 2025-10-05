dfcecc011ee12c143a8c3bed1e368cd100c6a4f7
Nick Masi <33795684+N-Masi@users.noreply.github.com>
2025-10-04 23:09:43 -0400

    start MBTA API notes

---
+++ Ttracker.py
@@ -0,0 +1,12 @@
+'''
+
+[route]=83 (83 bus)
+[stop]=2437
+.name = "Beacon St @ Cooney St"
+.id=2437
+
+to get predicted times for 83 arrival at lighty:
+https://api-v3.mbta.com/predictions?filter[route]=83&filter[stop]=2437
+
+
+'''
